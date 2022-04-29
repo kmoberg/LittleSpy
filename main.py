e9b96580f06b6025a59fb60730063f49ec9b5765
Karl Mathias Moberg <km@kmoberg.no>
2022-04-29 11:54:15 +0200

    Add basic data capture, basic checking of a specified URL.

--- main.py
+++ main.py
@@ -1,7 +1,61 @@
+import json
+import logging
+import pprint
+from dataclasses import dataclass
+
+import requests
+
+
+@dataclass
+class RequestObject:
+    status_code: int
+    request_size: int
+    request_headers: dict
+    request_body: dict
+    response_size: int
+    response_time: float
+    headers: dict
+
+
+def check_url(url, request_headers, request_body):
+    try:
+        r = requests.get(url, headers=request_headers, data=request_body)
+        return RequestObject(status_code=r.status_code,
+                             request_size=r.__sizeof__(),
+                             request_headers=request_headers,
+                             request_body=request_body,
+                             response_size=r.__sizeof__(),
+                             response_time=r.elapsed.total_seconds(),
+                             headers=dict(r.headers))
+    except requests.ConnectTimeout:
+        return "Connection Timeout"
+    except requests.ConnectionError:
+        return "Connection Error"
+
+
 def main():
-    print("Hello World!")
+
+    url = "https://nyartcc.org"
+    request_headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36",
+    }
+    request_body = {"key": "value"}
+
+
+    check = check_url(url, request_headers, request_body)
+
+    logging.debug(f'Status Code: {check.status_code}')
+    logging.debug(f'Request Size: {check.request_size}')
+    logging.debug(f'Request Headers: {check.request_headers}')
+    logging.debug(f'Request Body: {check.request_body}')
+    logging.debug(f'Response Size: {check.response_size}')
+    logging.debug(f'Response Time: {check.response_time}')
+    logging.debug(json.dumps(check.headers, indent=4))
 
 
 if __name__ == '__main__':
-    main()
+    level = logging.DEBUG
+    fmt = '[%(levelname)s] %(asctime)s - %(message)s'
+    logging.basicConfig(level=level, format=fmt)
 
+    main()
